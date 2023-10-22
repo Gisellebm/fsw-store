eb326024686beeb9c4c7b0b878036cfa6354b5bc
Giselle Brasil Macedo <gisellebrasil.cf@gmail.com>
2023-10-22 02:51:10 -0300

    feat: add Categories and CategoriesItem component

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,17 +1,24 @@
 "use client"
 
 import Image from "next/image"
+import Categories from "./components/categories"
 
 
 export default function Home() { 
-  return <div className="p-5">
-    <Image 
-    src="/banner-home-01.png"
-    width={0}
-    height={0}
-    className="h-auto w-full"
-    sizes="100vw"
-    alt="Até 55% esse mês!" 
-    />
-  </div>
+  return (
+    <div className="p-5">
+      <Image 
+      src="/banner-home-01.png"
+      width={0}
+      height={0}
+      className="h-auto w-full"
+      sizes="100vw"
+      alt="Até 55% esse mês!" 
+      />
+
+      <div className="mt-8">
+        <Categories />
+      </div>
+    </div>
+  )
 }
