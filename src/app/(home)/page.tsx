f3030deede67573fb50bd5555ed94dd4f128248c
Giselle Brasil Macedo <gisellebrasil.cf@gmail.com>
2023-10-22 18:02:46 -0300

    feat: add computeProductTotal Price helper function

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,10 +1,17 @@
-"use client"
-
 import Image from "next/image"
 import Categories from "./components/categories"
+import { prismaClient } from "@/lib/prisma"
+import ProductList from "./components/product-list";
 
 
-export default function Home() { 
+export default async function Home() { 
+  const deals = await prismaClient.product.findMany({
+    where: {
+      discountPercentage: {
+        gt: 0,
+      },
+    },
+  });
   return (
     <div className="p-5">
       <Image 
@@ -18,6 +25,10 @@ export default function Home() {
 
       <div className="mt-8">
         <Categories />
+      </div>
+
+      <div>
+        <ProductList products={deals} />
       </div>
     </div>
   )
