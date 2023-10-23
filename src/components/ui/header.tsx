960e1d9295fb1234f005a6c64f1c7d9c64d657d8
Giselle Brasil Macedo <gisellebrasil.cf@gmail.com>
2023-10-23 02:23:44 -0300

    feat: ensure user goes to home page on logo click

--- src/components/ui/header.tsx
+++ src/components/ui/header.tsx
@@ -95,9 +95,11 @@ const Header = () => {
 
         </Sheet>
 
-        <h1 className="font-semibold text-lg">
+        <Link href="/">
+            <h1 className="text-lg font-semibold">
             <span className="text-primary">FSW</span> Store
-        </h1>
+            </h1>
+      </Link>
 
         <Button size="icon" variant="outline">
             <ShoppingCartIcon />
