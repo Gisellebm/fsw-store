eee54e38a680f0b02c874263ffd51a41d878a7c1
Giselle Brasil Macedo <gisellebrasil.cf@gmail.com>
2023-10-23 00:05:03 -0300

    fix: ensure product prices are wrapped in one line

--- src/components/ui/product-item.tsx
+++ src/components/ui/product-item.tsx
@@ -10,7 +10,7 @@ interface ProductItemProps {
 const ProductItem = ({ product }: ProductItemProps) => {
     //console.log('product.totalPrice:', product.totalPrice);
     return (
-        <div className="flex flex-col gap-4 max-w-[156px]">
+        <div className="flex flex-col gap-4 max-w-[170px]">
             <div className="relative bg-accent rounded-lg h-[170px] w-[156px] flex items-center justify-center">
                 <Image 
                     src={product.imageUrls[0]} 
